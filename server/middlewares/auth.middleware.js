3acc9d8dbf1a87b32619b8801ea3abb22823b87a
gauravkrsingh321 <gauravkrsingh40@gmail.com>
2025-07-10 11:16:24 +0530

    auth controller added

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken'
+export const authenticate = async (req, res, next) => {
+    try {
+        const token = req.cookies.access_token
+        if (!token) {
+            return next(401, 'Unauthorized - Token Missing')
+        }
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+        if(!decodedToken) {
+            next(401, 'Unauthorized -  Invalid Token')
+        }
+        req.user = decodedToken
+        next();
+    } 
+    catch (error) {
+        next(500, error.message)
+    }
+}
